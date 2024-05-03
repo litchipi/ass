222e6d07f7bd9ed8f72a5e36aef3b3f28ab65620
Litchi Pi <litchi.pi@proton.me>
2024-05-03 11:24:54 +0200

    Fixup autocompletion source directory
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- autocomplete.sh
+++ autocomplete.sh
@@ -1,11 +1,14 @@
 CMD="assistant"
 
+SRC="$(realpath ${BASH_SOURCE[0]})"
+SRC="$(dirname $SRC)"
+
 function _assistant_complete() {
     local cur=${COMP_WORDS[COMP_CWORD]}
     local prev=${COMP_WORDS[COMP_CWORD-1]}
 
     args=$(IFS=" "; echo "${COMP_WORDS[*]}")
-    res=$(python3 ./lib/cli.py $args)
+    res=$(python3 "$SRC/lib/cli.py" $args)
     COMPREPLY=( $(compgen -W "$res" -- $cur) )
     return 0
 }
