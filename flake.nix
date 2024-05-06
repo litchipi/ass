f4fe459057fe0175459e86c210cac3c86a434b2c
Litchi Pi <litchi.pi@proton.me>
2024-05-06 21:36:30 +0200

    Add flake on project root for dev dependencies
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import inputs.nixpkgs { inherit system; };
+
+    python_version = pkgs.python310;
+    pythonpkg = python_version.withPackages (p: with p; [
+      pydbus
+      pygobject3
+      gst-python
+      pydub
+    ]);
+
+    deps = with pkgs; [
+      pythonpkg
+      mpg123
+    ];
+
+  in {
+    devShells.default = pkgs.mkShell {
+      buildInputs = deps;
+      PYTHONPATH = "${pythonpkg}/${pythonpkg.sitePackages}:$PYTHONPATH";
+      LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib$LD_LIBRARY_PATH";
+    };
+  });
+}
