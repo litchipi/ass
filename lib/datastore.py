6c5d868dda3efbce7bb43f3e060988fbf021803a
Litchi Pi <litchi.pi@proton.me>
2024-05-09 13:22:07 +0200

    Split datastore into data and cache dir
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- lib/datastore.py
+++ lib/datastore.py
@@ -2,25 +2,33 @@ import os
 import sys
 
 class Datastore:
-    def __init__(self, subd, root=None):
-        if root is None:
-            self.root = os.path.abspath(
+    def __init__(self, *subd, data_root=None, cache_root=None):
+        if data_root is None:
+            self.data_root = os.path.abspath(
                 os.path.join(os.path.expanduser("~/.local/share"), subd)
             )
-        os.makedirs(self.root, exist_ok=True)
-        print("Data stored under {}".format(self.root))
+        else:
+            self.data_root = data_root
+        os.makedirs(self.data_root, exist_ok=True)
+
+        if cache_root is None:
+            self.cache_root = os.path.abspath(
+                os.path.join(os.path.expanduser("~/.cache"), subd)
+            )
+        else:
+            self.cache_root = cache_root
+        os.makedirs(self.cache_root, exist_ok=True)
 
     def cache_path(self, *path):
-        return os.path.join(self.root, "cache", *path)
+        return os.path.join(self.cache_root, *path)
 
     def data_path(self, *path):
-        return os.path.join(self.root, "data", *path)
+        return os.path.join(self.data_root, *path)
 
     def edit_data(self, *path):
-        fpath = os.path.join(self.root, "data", *path)
         if not os.getenv("EDITOR"):
             raise Exception("EDITOR environment variable not set")
-        os.system(f"$EDITOR {fname}")
+        os.system("$EDITOR {}".format(self.data_path(*path)))
 
 def setup_edit(subp):
     subp.add_argument("category", help="Category of the file", type=str)
